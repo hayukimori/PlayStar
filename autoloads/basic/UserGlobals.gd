fe79376ed644fa83fac26dad71014048366d41b4
hayukimori <hayukimori@gmail.com>
2026-06-23 22:51:53 -0300

    fix(todos): added basic type hints

--- autoloads/basic/UserGlobals.gd
+++ autoloads/basic/UserGlobals.gd
@@ -5,16 +5,16 @@ const USER_CONFIG_PATH: String = "user://user_config.tres"
 const USER_DEFAULTS_PATH: String = "user://user_default.tres"
 const USER_PLAYLISTS_PATH: String = "user://playlists/"
 
-var current_defaults #TODO: Create UserDefaults Resource
-var current_config # TODO: Create ConfigModel Resource
+var current_defaults: UserDefaults
+var current_config: ConfigModel
 
 ## Gets config from user folder
-func get_config():
+func get_config() -> ConfigModel:
     var res # ConfigModel
     if FileAccess.file_exists(USER_CONFIG_PATH):
         res = ResourceLoader.load(USER_CONFIG_PATH)
     else:
-        res = null #TODO: Replace with ConfigModel.new()
+        res = ConfigModel.new()
         save_config(res)
     
     current_config = res
@@ -26,12 +26,12 @@ func save_config(config) -> void: ResourceSaver.save(config, USER_CONFIG_PATH)
 
 
 ## Gets user defaults
-func get_defaults(): #TODO: Add type hint UserDefaults
+func get_defaults() -> UserDefaults:
     var res # UserDefaults
     if FileAccess.file_exists(USER_CONFIG_PATH):
         res = ResourceLoader.load(USER_CONFIG_PATH)
     else:
-        res = null #TODO: Replace with UserDefaults.new()
+        res = UserDefaults.new()
         save_defaults(res)
     
     current_defaults = res
