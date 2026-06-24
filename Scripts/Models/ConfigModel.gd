293da74b7d7d4841d6d0ec32942d33bc64f0e9ed
hayukimori <69482956+hayukimori@users.noreply.github.com>
2026-06-23 22:24:43 -0300

    Revert "Feature/database"

--- Scripts/Models/ConfigModel.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name ConfigModel
-
-@export var scan_path: String = ""
-@export var ignore_unknown_artists: bool = false
-@export var last_scan_date: Dictionary = Time.get_date_dict_from_system()
-@export var start_discord_rp: bool = true
-@export var show_album_name: bool = true # Rich Presence
--- Scripts/Models/UserDefaults.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace PlayStar.Scripts.Models;
-[GlobalClass]
-public partial class UserDefaults : Resource
-{
-    [Export] public bool RandomMode { get; set; } = false;
-    [Export] public int RepeatMode { get; set; } = 0;
-}
