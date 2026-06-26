773628fe28d6e723e1ecc62227d7d83cb6639c29
hayukimori <hayukimori@gmail.com>
2026-06-26 01:31:17 -0300

    feat(buttons): Added logic to RepeatButton and ShuffleButton

--- Scripts/UI/Components/RepeatButton.gd
+++ Scripts/UI/Components/RepeatButton.gd
@@ -1,7 +1,54 @@
-class_name RepeatButton
 extends Button
+class_name RepeatButton
 
+signal toggle_repeat(state: Definitions.RepeatMode)
+
+@export_group("Button UI")
+@export var texture_off: ImageTexture
+@export var texture_rp_one: ImageTexture
+@export var texture_rp_queue: ImageTexture
+@export var default_texture: ImageTexture
+
+var current_state: Definitions.RepeatMode = Definitions.RepeatMode.OFF
 
 func _ready() -> void:
 	set_process(false)
 	set_physics_process(false)
+
+
+func load_state(state: Definitions.RepeatMode) -> void:
+	current_state = state
+	_change_texture(state)
+
+func _change_texture(state: Definitions.RepeatMode) -> void:
+	match state:
+		Definitions.RepeatMode.OFF:
+				icon = (
+						texture_off if texture_off else default_texture
+				)
+		Definitions.RepeatMode.REPEAT_ONE:
+				icon = (
+						texture_rp_one if texture_rp_one else default_texture
+			)
+		Definitions.RepeatMode.REPEAT_QUEUE:
+				icon = (
+						texture_rp_queue if texture_rp_queue else default_texture
+				)
+		_:
+			icon = default_texture
+
+
+func next_state() -> void:
+	var is_last_state = current_state >= (len(Definitions.RepeatMode) - 1)
+	var local_state = current_state
+
+	if is_last_state:
+		local_state = Definitions.RepeatMode.OFF
+	else:
+		local_state = (current_state + 1) as Definitions.RepeatMode
+
+	load_state(local_state)
+
+func _pressed() -> void:
+	next_state()
+	toggle_repeat.emit()
--- Scripts/UI/Components/ShuffleButton.gd
+++ Scripts/UI/Components/ShuffleButton.gd
@@ -1,7 +1,26 @@
 class_name ShuffleButton
 extends Button
 
+signal toggle_shuffle(state: bool)
+
+@export_group("Button UI")
+@export var texture_on: ImageTexture
+@export var texture_off: ImageTexture
 
 func _ready() -> void:
 	set_process(false)
 	set_physics_process(false)
+
+	if (icon != texture_on) or (icon != texture_off):
+		if texture_off: icon = texture_off
+
+func load_state(state: bool) -> void:
+	button_pressed = state
+
+func _change_texture(state: bool) -> void:
+	if texture_on and state: icon = texture_on
+	if texture_off and !state: icon = texture_off
+
+func _toggled(toggled_on: bool) -> void:
+	_change_texture(toggled_on)
+	toggle_shuffle.emit()
