7f31a6f82b25932c9d3b151d56e3b7243743d74f
hayukimori <hayukimori@gmail.com>
2026-06-27 05:11:22 -0300

    refactor(main_layout): Removed test call to config_control

--- Scripts/UI/Layout/main_layout.gd
+++ Scripts/UI/Layout/main_layout.gd
@@ -2,7 +2,4 @@ extends Control
 
 @export var config_control: Control
 
-func _ready() -> void:
-	if config_control:
-		get_tree().create_timer(2.0)
-		config_control.open()
+func _ready() -> void: pass
