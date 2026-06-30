0ae1b445b0bc4721c0d0bc13d54def0f233c215d
hayukimori <hayukimori@gmail.com>
2026-06-30 19:37:13 -0300

    refactor(AlbumButtonCovered): Emits signal when clicked

--- Scripts/UI/Components/AlbumButtonCovered.gd
+++ Scripts/UI/Components/AlbumButtonCovered.gd
@@ -9,47 +9,48 @@ var file_path: String
 var image_processed: bool = false
 
 func _ready() -> void:
-    if !album: queue_free(); return;
+	if !album: queue_free(); return;
 
-    file_path = album.Songs[0].FilePath
+	file_path = album.Songs[0].FilePath
 
-    set_process(false)
-    set_physics_process(false)
-    set_process_input(false)
+	set_process(false)
+	set_physics_process(false)
+	set_process_input(false)
 
 
-    ArtService.ArtReady.connect(_on_art_ready)
-    request_art()
+	ArtService.ArtReady.connect(_on_art_ready)
+	request_art()
 
 
 
 func request_art():
-    var key = file_path
-    var cached = ArtService.GetIfCached(key)
+	var key = file_path
+	var cached = ArtService.GetIfCached(key)
 
-    if cached:
-            album_art.texture = cached
-            image_processed = true
-    else:
-            ArtService.Request(key, file_path)
+	if cached:
+			album_art.texture = cached
+			image_processed = true
+	else:
+			ArtService.Request(key, file_path)
 
 
 func _on_art_ready(key, texture) -> void:
-    if key == album.Songs[0].FilePath:
-       album_art.texture = texture
+	if key == album.Songs[0].FilePath:
+	   album_art.texture = texture
 
 
 func self_destroy() -> void:
-    if image_processed: queue_free(); return;
+	if image_processed: queue_free(); return;
 
-    self.hide()
-    var delete_timer = Timer.new()
-    delete_timer.wait_time = .35
-    delete_timer.one_shot = false
+	self.hide()
+	var delete_timer = Timer.new()
+	delete_timer.wait_time = .35
+	delete_timer.one_shot = false
 
-    add_child(delete_timer)
-    delete_timer.start()
+	add_child(delete_timer)
+	delete_timer.start()
 
 
 func _pressed() -> void:
-    print("Entering album: ", album.AlbumName)
+	SignalBus.emit_show_album(album)
+	print("Entering album: ", album.AlbumName)
