4445c0873752015ba3230a9dbd4438b01d2f061d
hayukimori <hayukimori@gmail.com>
2026-07-04 16:07:00 -0300

    fix(ConfigModel): set use_native_window=true as default config\n - Removed unused variable 'scan_path'

--- Scripts/Models/ConfigModel.gd
+++ Scripts/Models/ConfigModel.gd
@@ -1,10 +1,9 @@
 extends Resource
 class_name ConfigModel
 
-@export var scan_path: String = "" # OUTDATED
 @export var scan_paths: Array[String] = []
 @export var ignore_unknown_artists: bool = false
 @export var last_scan_date: Dictionary = Time.get_date_dict_from_system()
 @export var start_discord_rp: bool = true
 @export var show_album_name: bool = true # Rich Presence
-@export var use_native_window: bool = false
+@export var use_native_window: bool = true
