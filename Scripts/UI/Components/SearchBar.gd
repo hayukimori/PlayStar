bde8714491e8ccc8d8fcfa7776a66d4c06c63b68
hayukimori <hayukimori@gmail.com>
2026-08-12 01:40:16 -0300

    Include MusicBrainz metadata in search results and fix search bug

--- Scripts/UI/Components/SearchBar.gd
+++ Scripts/UI/Components/SearchBar.gd
@@ -127,7 +127,7 @@ func _do_search():
 			results = search_repo.SearchArtists(_pending_text)
 		SearchMode.ALBUM:
 			results = search_repo.SearchAlbums(_pending_text)
-		_: search_repo.Search(_pending_text)
+		_: results = search_repo.Search(_pending_text)
 
 	if search_mode == SearchMode.SONGS:
 		var term := _pending_text.to_lower()
